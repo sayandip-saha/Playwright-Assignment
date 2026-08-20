ec2f8206adfbc21fdf029606200050e00047b70d
sayandip-saha <sayandip.saha@kawach.ai>
2026-08-20 17:10:04 +0530

    More Files Added

---
+++ tests/Functional/footer.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+import users from "../../data/users.js";
+
+const BASE_URL = "https://app.thetestingacademy.com/playwright/ttacart/inventory";
+
+test("show footer icons", async ({ page }) => {
+  await page.goto(BASE_URL);
+
+  await expect(page.getByRole("link", { name: "Twitter" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Facebook" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "LinkedIn" })).toBeVisible();
+});
